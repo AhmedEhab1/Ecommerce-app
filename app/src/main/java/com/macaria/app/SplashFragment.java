21036a31f3772f9b092a540667cdc7cf7e291dc0
AhmedEhab1 <45875687+AhmedEhab1@users.noreply.github.com>
2022-11-14 13:43:56 +0200

    init project

--- app/src/main/java/com/macaria/app/SplashFragment.java
+++ app/src/main/java/com/macaria/app/SplashFragment.java
@@ -3,7 +3,9 @@ package com.macaria.app;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,16 @@ import android.view.ViewGroup;
 import com.bumptech.glide.Glide;
 import com.macaria.app.databinding.SplashFragmentBinding;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class SplashFragment extends Fragment {
     private SplashFragmentBinding binding;
 
+    @Inject
+    Handler handler;
 
     public SplashFragment() {
         // Required empty public constructor
@@ -25,12 +34,11 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = SplashFragmentBinding.inflate(inflater, container, false);
-        // Inflate the layout for this fragment
-//
-//        Glide.with(this)
-//                .load(R.raw.splash)
-//                .into(binding.imageView);
-
+        handler.postDelayed(this::startProductsView, 1000);
         return binding.getRoot();
+    }
+
+    private void startProductsView() {
+        Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment);
     }
 }
---
+++ app/src/main/java/com/macaria/app/di/ActivityModule.java
@@ -0,0 +1,23 @@
+package com.macaria.app.di;
+
+
+import android.os.Handler;
+import android.os.Looper;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+
+@Module
+@InstallIn(ActivityComponent.class)
+public class ActivityModule {
+
+    @Provides
+    public Handler providerHandler() {
+        return new Handler(Looper.getMainLooper());
+    }
+
+
+
+}
